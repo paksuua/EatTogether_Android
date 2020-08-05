dd57bb390ec644e90629df4d4bb2cb911b458130
paksuua <paksuua@naver.com>
2020-08-06 00:41:31 +0900

    0806_UI completed.

--- app/src/main/java/com/example/eattogether_neep/UI/Activity/WaitingActivity.kt
+++ app/src/main/java/com/example/eattogether_neep/UI/Activity/WaitingActivity.kt
@@ -1,23 +1,30 @@
 package com.example.eattogether_neep.UI.Activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import com.bumptech.glide.Glide
 import com.example.eattogether_neep.R
+import kotlinx.android.synthetic.main.activity_ranking.*
 import kotlinx.android.synthetic.main.activity_waiting.*
 
+
 class WaitingActivity : AppCompatActivity() {
-    private lateinit var img_rotate: ImageView
     var second: Int=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_waiting)
 
-        img_rotate=findViewById(R.id.img_rotate)
-        animationRotate()
+        // loading git
+        Glide.with(this).load(R.drawable.loading).into(img_rotate)
+
 
         cst_wait.setOnClickListener {
             val intent = Intent(this, LaunchActivity::class.java)
